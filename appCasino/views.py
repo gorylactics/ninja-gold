4d678782451da52c6b842baf98341ab902c9ccbb
Adrian <adrian.miranda.r@gmail.com>
2021-08-04 22:29:51 -0400

    refactorizacion con uso de diccionario y creacion de variable de sesion para el num de intentos

--- appCasino/views.py
+++ appCasino/views.py
@@ -7,6 +7,7 @@ def index(request):
     return render(request, 'appCasino/index.html')
 
 def procesar(request):
+    
     if request.POST['opcion'] == 'granja':
         numero = random.randint(10, 20)
     
@@ -27,28 +28,28 @@ def procesar(request):
     if 'contador' in request.session:
         request.session['contador'] = request.session['contador'] + numero
     else:
-        request.session['contador'] = numero
-    
+        request.session['contador'] = numero 
+        
     if not ('log' in request.session):
         request.session['log'] = []
-    
+
+    if 'intento' in request.session:
+        request.session['intento'] = request.session['intento'] + 1
+    else:
+        request.session['intento'] = 1
 
     informacion_a_entregar = {
+        
+        'jugada'    : request.session['intento'],
         'ubicacion' : request.POST['opcion'],
-        'fecha' :  time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()),
-        # 'textLog' : (f'ganaste: {numero} monedas desde: {ubicacion} {fecha}')
+        'fecha'     : time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()),
+        'monedas'   : numero,
+        'palabra'   : 'ganaste' if numero > 0 else 'perdiste',
+        'color'     : 'verde' if numero > 0 else 'rojo',
     }
 
-    # ubicacion = request.POST['opcion']
-    # fecha =  time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-    # textLog = (f'ganaste: {numero} monedas desde: {ubicacion} {fecha}')
-
     request.session['log'].append(informacion_a_entregar)
-    # request.session['log'].append(textLog)
     request.session.save()
-
-    
-
     return redirect('/')
 
 # def reseteo(request):
@@ -56,11 +57,6 @@ def procesar(request):
 #         request.session['contador'] = 0
 #     return redirect('')
 
-
-# def listado(request):
-#     listaDeInteracciones = []
-#     listaDeInteracciones.append(request.POST)
-#     return   redirect('/') mas meos eso
 
 
 
